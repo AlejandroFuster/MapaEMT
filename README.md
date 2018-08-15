adf659d5c148bd687bfb2c37da71554da40eb562
Unknown <alejandrofuster.1998@gmail.com>
2018-08-15 20:08:15 +0200

    Código Inicial

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# MapaEMT
-🚌Visualización de la localización de los autobuses de la EMT de Málaga con Leaflet🍃
+# Autobuses EMT de Málaga
+
+Ejemplo de como visualizar mediante Leaflet la ubicación de los distintos autobuses, con los datos proporcionados por el Excmo. Ayto. de Málaga.
+
+
+## ¿Cómo usarlo?
+
+Puedes abrir el "index.html" que se encuentra en la carpeta "src" para poder visualizar en cualquier navegador el mapa.
+
+### Datos utilizados
+
+Se han usado los datos del [Excmo. Ayto. de Málaga](https://datosabiertos.malaga.eu/dataset/ubicaciones-de-autobuses-emt-en-tiempo-real/resource/9bc05288-1c11-4eec-8792-d74b679c8fcf/). En la API, se da el siguiente ejemplo (de gran utilidad) de uso en JavaScript con jQuery
+
+```
+  var data = {
+    resource_id: '9bc05288-1c11-4eec-8792-d74b679c8fcf', //ID del recurso
+  };
+  $.ajax({
+    url: 'http://datosabiertos.malaga.eu/api/3/action/datastore_search',
+    data: data,
+    dataType: 'jsonp',
+    success: function(data) {
+      alert('Total results found: ' + data.result.total)
+    }
+  });
+```
+
+En el código de este ejemplo, se puede ver una manera tratar los datos para poder implementarse de manera óptima en Leaflet.
+
+## Colaborar
+
+¡Siéntete libre de abrir cualquier issue o colaborar con el proyecto de cualquier manera! Cualquier aportación será bienvenida. 
+
+## Autor
+
+* **Alejandro Fuster** - [AlejandroFuster](https://github.com/AlejandroFuster)
+
+## Reconocimientos
+
+* [Datos abiertos Ayto. Málaga](https://datosabiertos.malaga.eu/)
+
---
+++ src/script.js
@@ -0,0 +1,51 @@
+var url = 'https://datosabiertos.malaga.eu/api/3/action/datastore_search?resource_id=9bc05288-1c11-4eec-8792-d74b679c8fcf';
+var jsonFeatures = [];
+var i = 0;
+var map = L.map("map");
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | Ejemplo de <a href="https://github.com/AlejandroFuster">Alejandro Fuster</a>'
+}).addTo(map);
+map.setView([36.721261, -4.4212655]);
+map.setZoom(14)
+
+$.ajax({
+    url: url,
+    method: 'GET',
+    async: false
+}).done(function(result) {
+    // Array de datos de JSON
+    var data = result.result.records;
+    // Iteramos
+    data.forEach(function(point) {
+            var feature = {
+                type: 'Feature',
+                properties: point,
+                geometry: {
+                    type: 'Point',
+                    coordinates: [point.lon, point.lat]
+                }
+            };
+            jsonFeatures.push(feature);        
+    });
+}).fail(function(error) {
+    console.error(error);
+});
+
+var geoJson = {
+    type: 'FeatureCollection',
+    features: jsonFeatures
+};
+
+
+var coordinates = L.geoJson(geoJson,{
+  onEachFeature: function (feature, layer) {
+    layer.bindPopup('<h1>'+feature.properties._id + '</h1>'
+    +'<p>C&oacute;digo de l&iacute;nea: '+feature.properties.codLinea +'<br>'
+    +'C&oacute;digo de bus: '+feature.properties.codBus +'<br>'
+    +'C&oacute;digo de parada inicial: '+feature.properties.codParIni +'<br>'
+    +'Sentido: '+feature.properties.sentido +'<br>'
+    +'&Uacute;ltima actualizaci&oacute;n: '+feature.properties.last_update+'</p>');
+  }}).addTo(map);
+
+
